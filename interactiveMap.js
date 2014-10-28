04237bbe973ab3a124579f50434fabb5a4f807ae
Simon Zou <simon.zou@gmail.com>
2014-10-28 13:21:19 -0700

    indentation, added div to populate

--- interactiveMap.js
+++ interactiveMap.js
@@ -1,55 +1,56 @@
+var map = new Datamap(
+	{
+		element: document.getElementById('container'),
+		scope: 'world',
+	    fills: {
+	        'USA': '#1f77b4',
+	        'HTI': '#9467bd',
+	        'JPN': '#ff7f0e',
+	        'CHN': '#2ca02c',
+	        'IND': '#e377c2',
+	        'GBR': '#8c564b',
+	        'FRA': '#d62728',
+	        'PAK': '#7f7f7f',
+	        defaultFill: 'gray'
+	    },
+	    data: {
+	        'JPN': {fillKey: 'JPN'},
+	        'HTI': {fillKey: 'HTI'},
+	        'CHN': {fillKey: 'CHN'},
+	        'IND': {fillKey: 'IND'},
+	        'GBR': {fillKey: 'GBR'},
+	        'FRA': {fillKey: 'FRA'},
+	        'PAK': {fillKey: 'PAK'},
+	        'USA': {fillKey: 'USA'}
+	    },
+	        done: function(datamap) {
+            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography, data) {
+            	console.log(data.name);
+            	console.log(geography.properties);
+            });
+        },
 
-    var map = new Datamap(
-    	{
-    		element: document.getElementById('container'),
-    		scope: 'world',
-		    fills: {
-		        'USA': '#1f77b4',
-		        'HTI': '#9467bd',
-		        'JPN': '#ff7f0e',
-		        'CHN': '#2ca02c',
-		        'IND': '#e377c2',
-		        'GBR': '#8c564b',
-		        'FRA': '#d62728',
-		        'PAK': '#7f7f7f',
-		        defaultFill: 'gray'
-		    },
-		    data: {
-		        'JPN': {fillKey: 'JPN'},
-		        'HTI': {fillKey: 'HTI'},
-		        'CHN': {fillKey: 'CHN'},
-		        'IND': {fillKey: 'IND'},
-		        'GBR': {fillKey: 'GBR'},
-		        'FRA': {fillKey: 'FRA'},
-		        'PAK': {fillKey: 'PAK'},
-		        'USA': {fillKey: 'USA'}
-		    },
- 	        done: function(datamap) {
-	            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography, data) {
-	            	console.log(data.name);
-	            	console.log(geography.properties);
-	            });
-	        },
-
-	        geographyConfig: {
-            popupTemplate: function(geo, data) {
-            	if (geo.properties.name == "United States of America")
-                {
-                	return ['<div class="hoverinfo">','Country: '+
-                    geo.properties.name + '<br>'+ 'Professor: ','</div>'].join('');}
-             
-            }
+        geographyConfig: {
+        popupTemplate: function(geo, data) {
+        	if (geo.properties.name == "United States of America")
+            {
+            	return ['<div class="hoverinfo">','Country: '+
+                geo.properties.name + '<br>'+ 'Professor: ','</div>'].join('');}
+         
         }
+    }
 
-    	});
+	});
 
 
-    (function() {
-        $("h1").flicker();
-    }());
+(function() {
+    $("h1").flicker();
+}());
 
-    $(".CAN").on('click', function(){
-    	alert("hello");
-    })
+
+
+$(".CAN").on('click', function(){
+	alert("hello");
+})
 
 console.log(story_data);
